ce8c229be44305b4b7a4a7f34677fe9107248f3b
omuen <woo@omuen.com>
2020-03-25 14:11:05 +0800

    From VS-Code

--- history/GuessNumbers.py
+++ history/GuessNumbers.py
@@ -1,6 +1,6 @@
 import random
-g=random.randint(1,100)
-f=int(input('Guess a number between 1 and 100 : '))
+g=random.randint(1,1000)
+f=int(input('Guess a number between 1 and 1000 : '))
 while f != g:
     if f>g:
         print(f,' was too high')
