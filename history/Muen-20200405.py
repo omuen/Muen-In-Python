2fc8da61dd02a0d88560d24e3b89996c34b1bbb8
omuen <woo@omuen.com>
2020-04-05 16:54:26 +0800

    APPLES......

---
+++ history/Muen-20200405.py
@@ -0,0 +1,34 @@
+apples = [
+  "red","green","bad","red","hole","red",
+  "yellow",'bad','green','hole','bad','yellow',
+  'hole','yellow','hole','green','green'
+]
+r=0
+y=0
+h=0
+b=0
+g=0
+t=0
+for apple in apples:
+  t=t+1
+  if apple=='red':
+    r=r+1
+  if apple=='bad':
+    b=b+1
+  if apple=='green':
+    g=g+1
+  if apple=='yellow':
+    y=y+1
+  if apple =='hole':
+    h=h+1
+print('hole=',h)
+print('red=',r)
+print('yellow=',y)
+print('bad=',b)
+print('green=',g)
+print('Total=',t)
+print(apples[4:6])
+print(apples[7:10])
+print('  Done')
+print('~ By Muen ~')
+print(------- ------- ------- ------ ------ ------ ------)
---
+++ history/apples.py
@@ -0,0 +1,12 @@
+apples = [
+  {"color":'red', "weight":100},
+  {"color":'red', "weight":300},
+  {"color":'green', "weight":50},
+  {"color":'red', "weight":500},
+  {"color":'red', "weight":200},
+  {"color":'red', "weight":800},
+  {"color":'red', "weight":700},
+]
+for apple in apples:
+  print(apple["color"])
+  print(apple["weight"])
