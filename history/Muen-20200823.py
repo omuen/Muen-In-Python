01eacfdd8ac70a5eb8f8dce5544c517343f26608
omuen <woo@omuen.com>
2020-08-23 20:37:25 +0800

    一大堆...

---
+++ history/Muen-20200823.py
@@ -0,0 +1,30 @@
+def I_AM():
+    print('MADE BY')
+    print("~~~~~~~~~~")
+    print(' MUEN')
+
+def jam(x,y,z):
+    if z==1:
+        return x+y
+    elif z==2:
+        return x*y
+
+
+
+cat=jam(1,2,1)
+print(cat)
+
+cat=jam(1728,1728,2)
+print(cat)
+print('  ')
+
+
+I_AM()
+I_AM()
+I_AM()
+I_AM()
+I_AM()
+I_AM()
+I_AM()
+I_AM()
+I_AM()
